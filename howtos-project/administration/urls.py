07b37e0134f4ee2a95c39e0152c9d276e9e70d08
tiveritz <lukas.walch@icloud.com>
2021-04-03 18:36:24 +0200

    [Feature] adds menu, base templates and How To's view

--- howtos-project/administration/urls.py
+++ howtos-project/administration/urls.py
@@ -4,4 +4,8 @@ from administration import views
 
 urlpatterns = [
     path('', views.dashboard, name = 'dashboard'),
+    path('dashboard', views.dashboard, name = 'dashboard'),
+    path('information', views.information, name = 'information'),
+    path('howtos', views.howtos, name = 'howtos'),
+    path('howtos_edit/<str:uri_id>/', views.howtos_edit, name = 'howtos_edit'),
 ]
--- howtos-project/administration/views.py
+++ howtos-project/administration/views.py
@@ -1,9 +1,42 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import requests
+from datetime import datetime
 
-
-def test(request):
-    return HttpResponse("Hello World")
 
 def dashboard(request):
-	return render(request, 'pages/dashboard.html')
+	return render(request, 'pages/dashboard.html', {'menu' : 'dashboard'})
+
+def howtos(request):
+	response = requests.get('https://api.tiveritz.at/hwts/v1/howtos')
+	howtos = response.json()
+
+	for howto in howtos:
+		howto['created'] = convert_api_time(howto['created'])
+		howto['updated'] = convert_api_time(howto['updated'])
+
+	return render(request, 'pages/howtos.html',
+	{
+		'menu' : 'howtos',
+		'howtos' : howtos
+	})
+
+def howtos_edit(request, uri_id):
+	response = requests.get('https://api.tiveritz.at/hwts/v1/howtos/' + uri_id)
+	howto = response.json()
+
+	return render(request, 'pages/howtos_edit.html', {
+		'menu' : 'howtos',
+		'howto' : howto
+	})
+
+def information(request):
+	return render(request, 'pages/information.html', {'menu' : 'information'})
+
+
+# Helper functions
+def convert_api_time(api_time):
+	api_time_format = '%Y-%m-%dT%H:%M:%S+00:00'
+	app_time_format = '%Y.%m.%d %H:%M'
+	time = datetime.strptime(api_time, api_time_format)
+	return time.strftime(app_time_format)
